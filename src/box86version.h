3e1b5003dfd99e27d0287bc75385aeee8b660041
ptitSeb <sebastien.chev@gmail.com>
2018-07-01 16:36:21 +0200

    Forgot those 2 headers

---
+++ src/box86version.h
@@ -0,0 +1,8 @@
+#ifndef __BOX86_VERSION_H_
+#define __BOX86_VERSION_H_
+
+#define BOX86_MAJOR 0
+#define BOX86_MINOR 0
+#define BOX86_REVISION 1
+
+#endif //__BOX86_VERSION_H_
---
+++ src/debug.h
@@ -0,0 +1,11 @@
+#ifndef __DEBUG_H_
+#define __DEBUG_H_
+
+extern int box86_debug;    // debug level
+#define DEBUG_NONE 0
+#define DEBUG_INFO 1
+#define DEBUG_DEBUG 2
+
+#define printf_debug(L, ...) if(L<=box86_debug) printf(__VA_ARGS__)
+
+#endif //__DEBUG_H_
