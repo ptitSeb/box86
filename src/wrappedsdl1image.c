c66a789bff2c86d0464ff7f3bbf0a842a99b7d86
ptitSeb <sebastien.chev@gmail.com>
2019-01-02 22:59:42 +0100

    Added sdli_image wrapper (should be complete)

---
+++ src/wrappedsdl1image.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <dlfcn.h>
+
+#include "wrappedlibs.h"
+
+#include "debug.h"
+#include "wrapper.h"
+#include "bridge.h"
+#include "library_private.h"
+#include "x86emu.h"
+
+const char* sdl1imageName = "libSDL_image-1.2.so.0";
+#define LIBNAME sdl1image
+
+#include "wrappedlib_init.h"
+
