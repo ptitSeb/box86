1f252eb661ef4d533cc5167989794118a8e0ff77
ptitSeb <sebastien.chev@gmail.com>
2019-01-02 23:04:25 +0100

    Completed most of sdl2image wrapper

--- src/wrappedsdl2image_private.h
+++ src/wrappedsdl2image_private.h
@@ -3,55 +3,56 @@
 DATA(__data_start, 4)
 DATA(_edata, 4)
 // _fini
-// IMG_Init
+GO(IMG_Init,iFi)
+GO(IMG_InvertAlpha,iFi)
+GO(IMG_Linked_Version,pFv)
+GO(IMG_Load,pFp)
+GO(IMG_LoadBMP_RW,pFp)
+GO(IMG_LoadCUR_RW,pFp)
+GO(IMG_LoadGIF_RW,pFp)
+GO(IMG_LoadICO_RW,pFp)
+GO(IMG_LoadJPG_RW,pFp)
+GO(IMG_LoadLBM_RW,pFp)
+GO(IMG_LoadPCX_RW,pFp)
+GO(IMG_LoadPNG_RW,pFp)
+GO(IMG_LoadPNM_RW,pFp)
+GO(IMG_LoadTGA_RW,pFp)
+GO(IMG_LoadTIF_RW,pFp)
+GO(IMG_LoadTyped_RW,pFpip)
+GO(IMG_LoadWEBP_RW,pFp)
+GO(IMG_LoadXCF_RW,pFp)
+GO(IMG_LoadXPM_RW,pFp)
+GO(IMG_LoadXV_RW,pFp)
+GO(IMG_Load_RW,pFpi)
+GO(IMG_Quit,vFv)
+GO(IMG_ReadXPMFromArray,pFp)
+GO(IMG_isBMP,iFp)
+GO(IMG_isCUR,iFp)
+GO(IMG_isGIF,iFp)
+GO(IMG_isICO,iFp)
+GO(IMG_isJPG,iFp)
+GO(IMG_isLBM,iFp)
+GO(IMG_isPCX,iFp)
+GO(IMG_isPNG,iFp)
+GO(IMG_isPNM,iFp)
+GO(IMG_isTIF,iFp)
+GO(IMG_isWEBP,iFp)
+GO(IMG_isXCF,iFp)
+GO(IMG_isXPM,iFp)
+GO(IMG_isXV,iFp)
 // IMG_InitJPG
 // IMG_InitPNG
 // IMG_InitTIF
 // IMG_InitWEBP
-// IMG_isBMP
-// IMG_isCUR
-// IMG_isGIF
-// IMG_isICO
-// IMG_isJPG
-// IMG_isLBM
-// IMG_isPCX
-// IMG_isPNG
-// IMG_isPNM
 // IMG_isSVG
-// IMG_isTIF
-// IMG_isWEBP
-// IMG_isXCF
-// IMG_isXPM
-// IMG_isXV
-// IMG_Linked_Version
-GO(IMG_Load, pFp)
-// IMG_LoadBMP_RW
-// IMG_LoadCUR_RW
-// IMG_LoadGIF_RW
-// IMG_LoadICO_RW
-// IMG_LoadJPG_RW
-// IMG_LoadLBM_RW
-// IMG_LoadPCX_RW
-// IMG_LoadPNG_RW
-// IMG_LoadPNM_RW
-// IMG_Load_RW
 // IMG_LoadSVG_RW
 // IMG_LoadTexture
 // IMG_LoadTexture_RW
 // IMG_LoadTextureTyped_RW
-// IMG_LoadTGA_RW
-// IMG_LoadTIF_RW
-// IMG_LoadTyped_RW
-// IMG_LoadWEBP_RW
-// IMG_LoadXCF_RW
-// IMG_LoadXPM_RW
-// IMG_LoadXV_RW
-GO(IMG_Quit, vFv)
 // IMG_QuitJPG
 // IMG_QuitPNG
 // IMG_QuitTIF
 // IMG_QuitWEBP
-// IMG_ReadXPMFromArray
 // IMG_SaveJPG
 // IMG_SaveJPG_RW
 // IMG_SavePNG
