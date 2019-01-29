586c0b75b3b2b4eea5da15deb47896aa0f0db861
ptitSeb <sebastien.chev@gmail.com>
2019-01-29 08:41:09 +0100

    And put a few x87 utility functions inline too

--- src/x87emu_private.c
+++ src/x87emu_private.c
@@ -9,29 +9,6 @@
 #include "x87emu_private.h"
 #include "x86run_private.h"
 
-void fpu_do_push(x86emu_t* emu)
-{
-    ++emu->fpu_stack;
-    if(emu->fpu_stack == 9) {// overflow
-        printf_log(LOG_NONE, "Error: %p: FPU Stack overflow\n", (void*)emu->old_ip);    // probably better to raise something
-        emu->quit = 1;
-        return;
-    }
-    emu->top = (emu->top-1)&7;
-}
-
-void fpu_do_pop(x86emu_t* emu)
-{
-    emu->top = (emu->top+1)&7;
-    --emu->fpu_stack;
-    if(emu->fpu_stack < 0) {// underflow
-        printf_log(LOG_NONE, "Error: %p: FPU Stack underflow\n", (void*)emu->old_ip);    // probably better to raise something
-        emu->quit = 1;
-        return;
-    }
-
-}
-
 void reset_fpu(x86emu_t* emu)
 {
     memset(emu->fpu, 0, sizeof(emu->fpu));
@@ -40,89 +17,6 @@ void reset_fpu(x86emu_t* emu)
     emu->sw.x16 = 0x0000;
     emu->top = 0;
     emu->fpu_stack = 0;
-}
-
-void fpu_fcom(x86emu_t* emu, double b)
-{
-    if(isnan(ST0.d) || isnan(b)) {
-        emu->sw.f.F87_C0 = 1;
-        emu->sw.f.F87_C2 = 1;
-        emu->sw.f.F87_C3 = 1;
-    } else if (isgreater(ST0.d, b)) {
-        emu->sw.f.F87_C0 = 0;
-        emu->sw.f.F87_C2 = 0;
-        emu->sw.f.F87_C3 = 0;
-    } else if (isless(ST0.d, b)) {
-        emu->sw.f.F87_C0 = 1;
-        emu->sw.f.F87_C2 = 0;
-        emu->sw.f.F87_C3 = 0;
-    } else {
-        emu->sw.f.F87_C0 = 0;
-        emu->sw.f.F87_C2 = 0;
-        emu->sw.f.F87_C3 = 1;
-    }
-}
-
-void fpu_fcomi(x86emu_t* emu, double b)
-{
-    RESET_FLAGS(emu);
-    if(isnan(ST0.d) || isnan(b)) {
-        emu->eflags.f.F_CF = 1;
-        emu->eflags.f.F_PF = 1;
-        emu->eflags.f.F_ZF = 1;
-    } else if (isgreater(ST0.d, b)) {
-        emu->eflags.f.F_CF = 0;
-        emu->eflags.f.F_PF = 0;
-        emu->eflags.f.F_ZF = 0;
-    } else if (isless(ST0.d, b)) {
-        emu->eflags.f.F_CF = 1;
-        emu->eflags.f.F_PF = 0;
-        emu->eflags.f.F_ZF = 0;
-    } else {
-        emu->eflags.f.F_CF = 0;
-        emu->eflags.f.F_PF = 0;
-        emu->eflags.f.F_ZF = 1;
-    }
-}
-
-double fpu_round(x86emu_t* emu, double d) {
-    if (!isfinite(d))
-        return d;
-    //switch(emu->cw)   // TODO: implement Rounding...
-    return round(d);
-}
-
-void fpu_fxam(x86emu_t* emu) {
-    emu->sw.f.F87_C1 = (ST0.l.upper<0)?1:0;
-    if(!emu->fpu_stack) {
-        emu->sw.f.F87_C3 = 1;
-        emu->sw.f.F87_C2 = 0;
-        emu->sw.f.F87_C0 = 1;
-        return;
-    }
-    if(isinf(ST0.d)) {  // TODO: Unsuported and denormal not analysed...
-        emu->sw.f.F87_C3 = 0;
-        emu->sw.f.F87_C2 = 1;
-        emu->sw.f.F87_C0 = 1;
-        return;
-    }
-    if(isnan(ST0.d)) {  // TODO: Unsuported and denormal not analysed...
-        emu->sw.f.F87_C3 = 0;
-        emu->sw.f.F87_C2 = 0;
-        emu->sw.f.F87_C0 = 1;
-        return;
-    }
-    if(ST0.d==0.0) {
-        emu->sw.f.F87_C3 = 1;
-        emu->sw.f.F87_C2 = 0;
-        emu->sw.f.F87_C0 = 0;
-        return;
-    }
-    // normal...
-    emu->sw.f.F87_C3 = 0;
-    emu->sw.f.F87_C2 = 1;
-    emu->sw.f.F87_C0 = 0;
-
 }
 
 void fpu_fbst(x86emu_t* emu, uint8_t* d) {
