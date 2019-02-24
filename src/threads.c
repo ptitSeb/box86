dd63e8cc86e5b23ad6ceb473f0eff461911558f0
ptitSeb <sebastien.chev@gmail.com>
2019-02-24 11:11:12 +0100

    Made thread_once mecanism much simpler

--- src/threads.c
+++ src/threads.c
@@ -60,19 +60,9 @@ int EXPORT my_pthread_create(x86emu_t *emu, void* t, void* attr, void* start_rou
 
 KHASH_MAP_INIT_INT(once, int)
 
-#define nb_once	16
-typedef void(*thread_once)(void);
+#define nb_dtor	16
 typedef void(*key_dtor)(void*);
-static x86emu_t *once_emu[nb_once] = {0};
-static x86emu_t *dtor_emu[nb_once] = {0};
-static void thread_once_callback(int n)
-{
-	if(once_emu[n]) {
-		RunCallback(once_emu[n]);
-		FreeCallback(once_emu[n]);
-		once_emu[n] = NULL;
-	}
-}
+static x86emu_t *dtor_emu[nb_dtor] = {0};
 static void key_dtor_callback(int n, void* a)
 {
 	if(dtor_emu[n]) {
@@ -84,10 +74,6 @@ static void key_dtor_callback(int n, void* a)
 void key_dtor_callback_##N(void* a) \
 { \
 	key_dtor_callback(N, a); \
-} \
-void thread_once_callback_##N() \
-{ \
-	thread_once_callback(N); \
 }
 GO(0)
 GO(1)
@@ -106,47 +92,33 @@ GO(13)
 GO(14)
 GO(15)
 #undef GO
-static const thread_once once_cb[nb_once] = {
-	 thread_once_callback_0, thread_once_callback_1, thread_once_callback_2, thread_once_callback_3
-	,thread_once_callback_4, thread_once_callback_5, thread_once_callback_6, thread_once_callback_7
-	,thread_once_callback_8, thread_once_callback_9, thread_once_callback_10,thread_once_callback_11
-	,thread_once_callback_12,thread_once_callback_13,thread_once_callback_14,thread_once_callback_15
-};
-static const key_dtor dtor_cb[nb_once] = {
+static const key_dtor dtor_cb[nb_dtor] = {
 	 key_dtor_callback_0, key_dtor_callback_1, key_dtor_callback_2, key_dtor_callback_3
 	,key_dtor_callback_4, key_dtor_callback_5, key_dtor_callback_6, key_dtor_callback_7
 	,key_dtor_callback_8, key_dtor_callback_9, key_dtor_callback_10,key_dtor_callback_11
 	,key_dtor_callback_12,key_dtor_callback_13,key_dtor_callback_14,key_dtor_callback_15
 };
-kh_once_t 	*once_map = NULL;
 // TODO: put all this in libpthread private stuff...
+static x86emu_t *once_emu = NULL;
+static uintptr_t once_fnc = 0;
+static void my_thread_once_callback()
+{
+	if(!once_emu)
+		return;
+	EmuCall(once_emu, once_fnc);
+}
 
 int EXPORT my_pthread_once(x86emu_t* emu, void* once, void* cb)
 {
-	// look if that once already is in map
-	pthread_mutex_lock(&emu->context->mutex_once);
-	if(!once_map) {
-		once_map = kh_init(once);
-	}
-	khint_t k;
-	k = kh_get(once, once_map, (uintptr_t)once);
-	if(k!=kh_end(once_map)) {
-			pthread_mutex_unlock(&emu->context->mutex_once);
-			return pthread_once(once, once_cb[kh_value(once_map, k)]);
-	}
-	// look for a free slot
-	for(int i=0; i<nb_once; ++i)
-		if(!once_emu[i]) {
-			once_emu[i] = AddCallback(emu, (uintptr_t)cb, 0, NULL, NULL, NULL, NULL);
-			int ret;
-			k = kh_put(once, once_map, (uintptr_t)once, &ret);	// add to map
-			kh_value(once_map, k) = i;
-			pthread_mutex_unlock(&emu->context->mutex_once);
-			return pthread_once(once, once_cb[i]);
-		}
-	printf_log(LOG_NONE, "Warning, no more slot on pthread_once");
+	pthread_mutex_lock(&emu->context->mutex_once);	// what if it deadlock here?
+
+	once_emu = emu;
+	once_fnc = (uintptr_t)cb;
+	int ret = pthread_once(once, my_thread_once_callback);
+	once_emu = NULL;
+
 	pthread_mutex_unlock(&emu->context->mutex_once);
-	return 0;
+	return ret;
 }
 
 EXPORT int my_pthread_key_create(x86emu_t* emu, void* key, void* dtor)
@@ -154,7 +126,7 @@ EXPORT int my_pthread_key_create(x86emu_t* emu, void* key, void* dtor)
 	if(!dtor)
 		return pthread_key_create((pthread_key_t*)key, NULL);
 	int n = 0;
-	while (n<nb_once) {
+	while (n<nb_dtor) {
 		if(!dtor_emu[n] || GetCallbackAddress(dtor_emu[n])==((uintptr_t)dtor)) {
 			if(!dtor_emu[n]) 
 				dtor_emu[n] = AddCallback(emu, (uintptr_t)dtor, 1, NULL, NULL, NULL, NULL);
