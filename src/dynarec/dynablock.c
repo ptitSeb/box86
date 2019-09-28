5f91619de7ee4383f18d61ba1b60ce7e54560ef6
ptitSeb <sebastien.chev@gmail.com>
2019-09-28 16:20:52 +0200

    [DYNAREC] Put some atomic to lock concurent access to dynablock list

--- src/dynarec/dynablock.c
+++ src/dynarec/dynablock.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <errno.h>
+#include <stdatomic.h>
 
 #include "debug.h"
 #include "box86context.h"
@@ -21,11 +22,19 @@
 
 KHASH_MAP_INIT_INT(dynablocks, dynablock_t*)
 
+static _Atomic(int) dynalock;
+static int lock_inited = 0;
+
 
 dynablocklist_t* NewDynablockList()
 {
     dynablocklist_t* ret = (dynablocklist_t*)calloc(1, sizeof(dynablocklist_t));
     ret->blocks = kh_init(dynablocks);
+
+    if(!lock_inited) {
+        atomic_store(&dynalock, 0);
+        lock_inited = 1;
+    }
 
     return ret;
 }
@@ -51,10 +60,18 @@ void FreeDynablockList(dynablocklist_t** dynablocks)
 dynablock_t* DBGetBlock(box86context_t* context, uintptr_t addr, int create)
 {
     dynablocklist_t *dynablocks = context->dynablocks;
+    // use and atomic lock to avoid concurent access to dynablocks list
+    int tmp;
+    do {
+        tmp = 0;
+    } while(!atomic_compare_exchange_weak(&dynalock, &tmp, 1));
     khint_t k = kh_get(dynablocks, dynablocks->blocks, addr);
     // check if the block exist, that would be easy
-    if(k!=kh_end(dynablocks->blocks) && kh_exist(dynablocks->blocks, k))
+    if(k!=kh_end(dynablocks->blocks) && kh_exist(dynablocks->blocks, k)) {
+        atomic_store(&dynalock, 0);
         return kh_value(dynablocks->blocks, k);
+    }
+    atomic_store(&dynalock, 0);
     // Blocks doesn't exist. If creation is not allow, just return NULL
     if(!create)
         return NULL;
