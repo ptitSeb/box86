6cacba1a677b8fa389fd026d8a3410f9fedc3192
ptitSeb <sebastien.chev@gmail.com>
2019-09-29 15:45:07 +0200

    [DYNAREC] Added Call relative opcode

--- src/dynarec/dynarec_arm_pass3.c
+++ src/dynarec/dynarec_arm_pass3.c
@@ -24,14 +24,15 @@ static void printf_x86_instruction(zydis_dec_t* dec, instruction_x86_t* inst, co
     if(ip[0]==0xcc && ip[1]=='S' && ip[2]=='C') {
         uint32_t a = *(uint32_t*)(ip+3);
         if(a==0) {
-            dynarec_log(LOG_NONE, "0x%p: Exit x86emu\n", (void*)ip);
+            dynarec_log(LOG_NONE, "%p: Exit x86emu\n", (void*)ip);
         } else {
-            dynarec_log(LOG_NONE, "0x%p: Native call to %p\n", (void*)ip, (void*)a);
+            dynarec_log(LOG_NONE, "%p: Native call to %p\n", (void*)ip, (void*)a);
         }
     } else {
         if(dec) {
-            dynarec_log(LOG_NONE, "%s\n", DecodeX86Trace(dec, inst->addr));
+            dynarec_log(LOG_NONE, "%p: %s\n", inst->addr, DecodeX86Trace(dec, inst->addr));
         } else {
+            dynarec_log(LOG_NONE, "%p: ", inst->addr);
             for(int i=0; i<inst->size; ++i)
                 dynarec_log(LOG_NONE, "%02X ", ip[i]);
             dynarec_log(LOG_NONE, " %s\n", name);
