8f2da8fe379755dfec0c8e75341ea1645ce9569b
ptitSeb <sebastien.chev@gmail.com>
2019-10-02 08:42:30 +0200

    [DYNAREC] Created and changed some macro to simplify opcode writing

--- src/dynarec/dynarec_arm_pass1.c
+++ src/dynarec/dynarec_arm_pass1.c
@@ -27,7 +27,7 @@
 #define FINI    
 #define MESSAGE(A, ...)  
 #define EMIT(A)     
-#define FLAGS(A)    dyn->insts[ninst].x86.flags = A
+#define UFLAGS    dyn->insts[ninst].x86.flags = X86_FLAGS_CHANGE
 #define NEW_INST \
     dyn->insts[ninst].x86.addr = ip; \
     if(ninst) dyn->insts[ninst-1].x86.size = dyn->insts[ninst].x86.addr - dyn->insts[ninst-1].x86.addr;
