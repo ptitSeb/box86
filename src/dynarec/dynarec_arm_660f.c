51c0afa1a19df633fd4038fba24b7701e7ad17f5
ptitSeb <sebastien.chev@gmail.com>
2019-10-12 19:18:14 +0200

    [DYNAREC] Added shld/shrd 16 opcode

--- src/dynarec/dynarec_arm_660f.c
+++ src/dynarec/dynarec_arm_660f.c
@@ -27,7 +27,7 @@ uintptr_t dynarec660f(dynarec_arm_t* dyn, uintptr_t addr, int ninst, int* ok, in
 {
     uintptr_t ip = addr-2;
     uint8_t opcode = F8;
-    uint8_t nextop;
+    uint8_t nextop, u8;
     int32_t i32, i32_;
     int16_t i16;
     uint16_t u16;
@@ -41,6 +41,42 @@ uintptr_t dynarec660f(dynarec_arm_t* dyn, uintptr_t addr, int ninst, int* ok, in
             FAKEED;
             break;
         
+        case 0xA4:
+        case 0xA5:
+            nextop = F8;
+            if(opcode==0xA4) {
+                INST_NAME("SHLD Ew, Gw, Ib");
+                u8 = F8;
+                MOVW(x3, u8);
+            } else {
+                INST_NAME("SHLD Ew, Gw, CL");
+                UXTB(x3, xECX, 0);
+            }
+            GETEWW(x12, x1);
+            GETGW(x2);
+            CALL(shld16, x1, (wback?(1<<wback):0));
+            EWBACKW(x1);
+            UFLAGS(1);
+            break;
+
+        case 0xAC:
+        case 0xAD:
+            nextop = F8;
+            if(opcode==0xA4) {
+                INST_NAME("SHRD Ew, Gw, Ib");
+                u8 = F8;
+                MOVW(x3, u8);
+            } else {
+                INST_NAME("SHRD Ew, Gw, CL");
+                UXTB(x3, xECX, 0);
+            }
+            GETEWW(x12, x1);
+            GETGW(x2);
+            CALL(shrd16, x1, (wback?(1<<wback):0));
+            EWBACKW(x1);
+            UFLAGS(1);
+            break;
+
         default:
             *ok = 0;
             DEFAULT;
