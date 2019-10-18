efa5701654e70a0e9a039c77921100c82a172904
ptitSeb <sebastien.chev@gmail.com>
2019-10-18 13:42:21 +0200

    [DYNAREC] So this is still needed (issue can be ssen on pico8 0.11 with disapearing menu in splore)

--- src/dynarec/dynarec_arm_pass1.h
+++ src/dynarec/dynarec_arm_pass1.h
@@ -5,7 +5,7 @@
     if(ninst) dyn->insts[ninst-1].x86.size = dyn->insts[ninst].x86.addr - dyn->insts[ninst-1].x86.addr;
 #define MESSAGE(A, ...)  
 #define EMIT(A)     
-#define UFLAGS(A)   dyn->insts[ninst].x86.flags = X86_FLAGS_CHANGE
+#define UFLAGS(A)   dyn->insts[ninst+(A?1:0)].x86.flags = X86_FLAGS_CHANGE
 #define USEFLAG(A)  dyn->insts[ninst].x86.flags = X86_FLAGS_USE
 #define JUMP(A)     dyn->insts[ninst].x86.jmp = A
 #define NEW_INST \
