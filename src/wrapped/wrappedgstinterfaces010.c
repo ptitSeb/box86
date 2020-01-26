f992b50444115002fbc1ae2612ce2d2da1f0e9b3
ptitSeb <sebastien.chev@gmail.com>
2020-01-26 15:29:45 +0100

    Added empty wrapped libs gstreamer-0.10 and gstinterfaces-0.10

---
+++ src/wrapped/wrappedgstinterfaces010.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <dlfcn.h>
+
+#include "wrappedlibs.h"
+
+#include "wrapper.h"
+#include "bridge.h"
+#include "librarian/library_private.h"
+#include "x86emu.h"
+
+const char* gstinterfaces010Name = "libgstinterfaces-0.10.so.0";
+#define LIBNAME gstinterfaces010
+
+#define CUSTOM_INIT \
+    lib->priv.w.needed = 1; \
+    lib->priv.w.neededlibs = (char**)calloc(lib->priv.w.needed, sizeof(char*)); \
+    lib->priv.w.neededlibs[0] = strdup("libgstreamer-0.10.so.0");
+
+#include "wrappedlib_init.h"
