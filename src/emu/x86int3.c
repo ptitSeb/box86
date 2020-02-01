bfbf7c2d102bdf6fd21ac8d4d1a948f1280c0393
ptitSeb <sebastien.chev@gmail.com>
2020-02-01 15:52:55 +0100

    Added better debug for XCreateWindow

--- src/emu/x86int3.c
+++ src/emu/x86int3.c
@@ -157,6 +157,8 @@ void x86Int3(x86emu_t* emu)
                     post = 3;
                 } else  if(strstr(s, "fprintf")==s) {
                     snprintf(buff, 255, "%04d|%p: Calling %s(%08X, \"%s\", ...)", tid, *(void**)(R_ESP), "fprintf", *(uint32_t*)(R_ESP+4), *(char**)(R_ESP+8));
+                } else if(strstr(s, "XCreateWindow")==s) {
+                    snprintf(buff, 255, "%04d|%p: Calling %s(%p, %p, %d, %d, %ud, %ud, %ud, %d, %ud, %p, %uld, %p)", tid, *(void**)(R_ESP), "XCreateWindow", *(void**)(R_ESP+4), *(void**)(R_ESP+8), *(int*)(R_ESP+12), *(int*)(R_ESP+16), *(uint32_t*)(R_ESP+20), *(uint32_t*)(R_ESP+24), *(uint32_t*)(R_ESP+28), *(int32_t*)(R_ESP+32), *(uint32_t*)(R_ESP+36), *(void**)(R_ESP+40), *(uint32_t*)(R_ESP+44), *(void**)(R_ESP+48));
                 } else if(strstr(s, "XLoadQueryFont")==s) {
                     snprintf(buff, 255, "%04d|%p: Calling %s(%p, \"%s\")", tid, *(void**)(R_ESP), "XLoadQueryFont", *(void**)(R_ESP+4), *(char**)(R_ESP+8));
                 } else if(strstr(s, "pthread_mutex_lock")==s) {
