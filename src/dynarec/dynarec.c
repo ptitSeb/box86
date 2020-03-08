247622a6dbf9b30a0a96851b4207df14b85d2290
ptitSeb <sebastien.chev@gmail.com>
2020-03-08 16:36:26 +0100

    [DYNAREC] Small change in how JIT'd code in handled in the linker

--- src/dynarec/dynarec.c
+++ src/dynarec/dynarec.c
@@ -53,8 +53,8 @@ void* UpdateLinkTable(x86emu_t* emu, void** table, uintptr_t addr)
         #endif
         return arm_epilog;
     }
-    if(!block->block || block->parent->nolinker) {
-        // null block, but done, or block that doesn't allow linker: go to epilog, no linker here
+    if(!block->block) {
+        // null block, but done: go to epilog, no linker here
         #ifdef ARM
         r = arm_tableupdate(arm_epilog, addr, table);
         if(r) dynarec_log(LOG_DEBUG, "Linker: failed to set table data @%p, for emu=%p\n", table, emu);
@@ -64,13 +64,16 @@ void* UpdateLinkTable(x86emu_t* emu, void** table, uintptr_t addr)
         #endif
         return arm_epilog;
     }
-    #ifdef ARM
-    r = arm_tableupdate(block->block, addr, table);
-    if(r) dynarec_log(LOG_DEBUG, "Linker: failed to set table data @%p, for emu=%p\n", table, emu);
-    #else
-    table[0] = block->block;
-    table[1] = addr;
-    #endif
+    if(!block->parent->nolinker) {
+        // only update block if linker is allowed
+        #ifdef ARM
+        r = arm_tableupdate(block->block, addr, table);
+        if(r) dynarec_log(LOG_DEBUG, "Linker: failed to set table data @%p, for emu=%p\n", table, emu);
+        #else
+        table[0] = block->block;
+        table[1] = addr;
+        #endif
+    }
     return block->block;
 }
 #endif
