12098629963771679c7fd53c57c39f33f568bfb4
ptitSeb <sebastien.chev@gmail.com>
2020-03-16 17:56:57 +0100

    Fixed fprintf trace (in case it's in fact __fprintf_chk that is called)

--- src/emu/x86int3.c
+++ src/emu/x86int3.c
@@ -156,8 +156,10 @@ void x86Int3(x86emu_t* emu)
                     pu32 = *(uint32_t**)(R_ESP+4);
                     post = 3;
                 } else  if(strstr(s, "fprintf")==s) {
-                    pu32 = *(uint32_t**)(R_ESP+8);  // can be null sometime?
-                    snprintf(buff, 255, "%04d|%p: Calling %s(%08X, \"%s\", ...)", tid, *(void**)(R_ESP), "fprintf", *(uint32_t*)(R_ESP+4), pu32?*(char**)(R_ESP+8):"nil");
+                    pu32 = *(uint32_t**)(R_ESP+8);
+                    if(((uintptr_t)pu32)<0x5) // probably a __fprint_chk
+                        pu32 = *(uint32_t**)(R_ESP+12);
+                    snprintf(buff, 255, "%04d|%p: Calling %s(%08X, \"%s\", ...)", tid, *(void**)(R_ESP), "fprintf", *(uint32_t*)(R_ESP+4), pu32?((char*)(pu32)):"nil");
                 } else if(strstr(s, "XCreateWindow")==s) {
                     snprintf(buff, 255, "%04d|%p: Calling %s(%p, %p, %d, %d, %u, %u, %u, %d, %u, %p, %u, %p)", tid, *(void**)(R_ESP), "XCreateWindow", *(void**)(R_ESP+4), *(void**)(R_ESP+8), *(int*)(R_ESP+12), *(int*)(R_ESP+16), *(uint32_t*)(R_ESP+20), *(uint32_t*)(R_ESP+24), *(uint32_t*)(R_ESP+28), *(int32_t*)(R_ESP+32), *(uint32_t*)(R_ESP+36), *(void**)(R_ESP+40), *(uint32_t*)(R_ESP+44), *(void**)(R_ESP+48));
                 } else if(strstr(s, "XLoadQueryFont")==s) {
