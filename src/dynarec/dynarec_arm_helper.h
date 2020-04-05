cbe4ec30d1c023c8ed60c37a76f2bc0cec264db5
ptitSeb <sebastien.chev@gmail.com>
2020-04-05 20:35:22 +0200

    [DYNAREC] Fun with flags: optimize FCOMI emiter

--- src/dynarec/dynarec_arm_helper.h
+++ src/dynarec/dynarec_arm_helper.h
@@ -202,6 +202,8 @@
     i32 = (dyn->insts)?(dyn->insts[ninst].epilog-(dyn->arm_size+8)):0; \
     Bcond(cond, i32)
 
+#define IFX(A)  if(dyn->insts && (dyn->insts[ninst].x86.need_flags&(A)))
+
 // Generate FCOM with s1 and s2 scratch regs (the VCMP is already done)
 #define FCOM(s1, s2)    \
     VMRS_APSR();    /* 0b0100011100000000 */                                    \
@@ -223,12 +225,18 @@
     MOVW_COND(cEQ, s1, 0b100); /* zero */                   \
     MOVW_COND(cGT, s1, 0b000); /* greater than */           \
     MOVW_COND(cLO, s1, 0b001); /* less than */              \
-    UBFX(s2, s1, 0, 1);                                     \
-    STR_IMM9(s2, xEmu, offsetof(x86emu_t, flags[F_CF]));    \
-    UBFX(s2, s1, 1, 1);                                     \
-    STR_IMM9(s2, xEmu, offsetof(x86emu_t, flags[F_PF]));    \
-    UBFX(s2, s1, 2, 1);                                     \
-    STR_IMM9(s2, xEmu, offsetof(x86emu_t, flags[F_ZF]));    \
+    IFX(X_CF) {                                             \
+        UBFX(s2, s1, 0, 1);                                 \
+        STR_IMM9(s2, xEmu, offsetof(x86emu_t, flags[F_CF]));\
+    }                                                       \
+    IFX(X_PF) {                                             \
+        UBFX(s2, s1, 1, 1);                                 \
+        STR_IMM9(s2, xEmu, offsetof(x86emu_t, flags[F_PF]));\
+    }                                                       \
+    IFX(X_ZF) {                                             \
+        UBFX(s2, s1, 2, 1);                                 \
+        STR_IMM9(s2, xEmu, offsetof(x86emu_t, flags[F_ZF]));\
+    }                                                       \
     MOVW(s2, d_none);                                       \
     STR_IMM9(s2, xEmu, offsetof(x86emu_t, df));
 
