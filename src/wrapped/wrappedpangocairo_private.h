34ed9f03e18c6682abf86328ed36f838f8d93969
ptitSeb <sebastien.chev@gmail.com>
2020-06-07 15:54:04 +0200

    Added cairo_pango wrapped functions

--- src/wrapped/wrappedpangocairo_private.h
+++ src/wrapped/wrappedpangocairo_private.h
@@ -2,36 +2,36 @@
 #error meh!
 #endif
 
-//GO(pango_cairo_context_get_font_options, 
-//GO(pango_cairo_context_get_resolution, 
-//GO(pango_cairo_context_get_shape_renderer, 
-//GO(pango_cairo_context_set_font_options, 
-//GO(pango_cairo_context_set_resolution, 
-//GO(pango_cairo_context_set_shape_renderer, 
-//GO(pango_cairo_create_context, 
-//GO(pango_cairo_create_layout, 
-//GO(pango_cairo_error_underline_path, 
-//GO(pango_cairo_fc_font_get_type, 
-//GO(pango_cairo_fc_font_map_get_type, 
-//GO(pango_cairo_font_get_scaled_font, 
-//GO(pango_cairo_font_get_type, 
-//GO(pango_cairo_font_map_create_context, 
-//GO(pango_cairo_font_map_get_default, 
-//GO(pango_cairo_font_map_get_font_type, 
-//GO(pango_cairo_font_map_get_resolution, 
-//GO(pango_cairo_font_map_get_type, 
-//GO(pango_cairo_font_map_new, 
-//GO(pango_cairo_font_map_new_for_font_type, 
-//GO(pango_cairo_font_map_set_default, 
-//GO(pango_cairo_font_map_set_resolution, 
-//GO(pango_cairo_glyph_string_path, 
-//GO(pango_cairo_layout_line_path, 
-//GO(pango_cairo_layout_path, 
-//GO(pango_cairo_renderer_get_type, 
-//GO(pango_cairo_show_error_underline, 
-//GO(pango_cairo_show_glyph_item, 
-//GO(pango_cairo_show_glyph_string, 
-//GO(pango_cairo_show_layout, 
-//GO(pango_cairo_show_layout_line, 
-//GO(pango_cairo_update_context, 
-//GO(pango_cairo_update_layout, 
+GO(pango_cairo_context_get_font_options, pFp)
+GO(pango_cairo_context_get_resolution, dFp)
+//GOM(pango_cairo_context_get_shape_renderer, pFEpp)
+GO(pango_cairo_context_set_font_options, vFpp)
+GO(pango_cairo_context_set_resolution, vFpd)
+//GOM(pango_cairo_context_set_shape_renderer, vFEpppp)
+GO(pango_cairo_create_context, pFp)
+GO(pango_cairo_create_layout, pFp)
+GO(pango_cairo_error_underline_path, vFpdddd)
+GO(pango_cairo_fc_font_get_type, iFv)
+GO(pango_cairo_fc_font_map_get_type, iFv)
+GO(pango_cairo_font_get_scaled_font, pFp)
+GO(pango_cairo_font_get_type, iFv)
+GO(pango_cairo_font_map_create_context, pFp)
+GO(pango_cairo_font_map_get_default, pFv)
+GO(pango_cairo_font_map_get_font_type, iFp)
+GO(pango_cairo_font_map_get_resolution, dFp)
+GO(pango_cairo_font_map_get_type, iFv)
+GO(pango_cairo_font_map_new, pFv)
+GO(pango_cairo_font_map_new_for_font_type, pFi)
+GO(pango_cairo_font_map_set_default, vFp)
+GO(pango_cairo_font_map_set_resolution, vFpd)
+GO(pango_cairo_glyph_string_path, vFppp)
+GO(pango_cairo_layout_line_path, vFpp)
+GO(pango_cairo_layout_path, vFpp)
+GO(pango_cairo_renderer_get_type, iFv)
+GO(pango_cairo_show_error_underline, vFpdddd)
+GO(pango_cairo_show_glyph_item, vFppp)
+GO(pango_cairo_show_glyph_string, vFppp)
+GO(pango_cairo_show_layout, vFpp)
+GO(pango_cairo_show_layout_line, vFpp)
+GO(pango_cairo_update_context, vFpp)
+GO(pango_cairo_update_layout, vFpp)
