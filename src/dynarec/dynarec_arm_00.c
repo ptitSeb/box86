38740ce1c9114e18bcec2d39912d8ec40bfd8791
ptitSeb <sebastien.chev@gmail.com>
2020-07-08 22:46:43 +0200

    [DYNAREC] Try to avoid a segfault, bad mix with PltResolver that can hapens (should help #145)

--- src/dynarec/dynarec_arm_00.c
+++ src/dynarec/dynarec_arm_00.c
@@ -2020,7 +2020,7 @@ uintptr_t dynarec00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int ninst,
                 u8 = F8;
                 gd = xEAX+(u8&7);
                 MOV32(gd, addr-1);
-            } else if ((PK(i32+0)==0x8B) && (((PK(i32+1))&0xC7)==0x04) && (PK(i32+2)==0x24) && (PK(i32+3)==0xC3)) {
+            } else if (((addr+i32)>BOX86_ELF_ADDRESS) && (PK(i32+0)==0x8B) && (((PK(i32+1))&0xC7)==0x04) && (PK(i32+2)==0x24) && (PK(i32+3)==0xC3)) {
                 MESSAGE(LOG_DUMP, "Hack for Call x86.get_pc_thunk.reg\n");
                 u8 = PK(i32+1);
                 gd = xEAX+((u8&0x38)>>3);
