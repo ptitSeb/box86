20a680561a1a1ae781123dadfa3eb3bac070405d
ptitSeb <sebastien.chev@gmail.com>
2020-07-19 18:21:45 +0200

    Added wrapped libncurses.so.6 and libtsinfo.so.6 (based on .5 version)

---
+++ src/wrapped/wrappedlibncurses6.c
@@ -0,0 +1,81 @@
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dlfcn.h>
+
+#include "wrappedlibs.h"
+
+#include "debug.h"
+#include "wrapper.h"
+#include "bridge.h"
+#include "librarian/library_private.h"
+#include "x86emu.h"
+#include "emu/x86emu_private.h"
+#include "callback.h"
+#include "librarian.h"
+#include "box86context.h"
+#include "emu/x86emu_private.h"
+#include "myalign.h"
+
+const char* libncurses6Name = "libncurses.so.6";
+#define LIBNAME libncurses6
+
+// this is a simple copy of libncursesw wrapper. TODO: check if ok
+
+typedef int         (*iFppp_t)(void*, void*, void*);
+typedef int         (*iFpiip_t)(void*, int32_t, int32_t, void*);
+
+#define SUPER() \
+    GO(mvwprintw, iFpiip_t)
+
+typedef struct libncurses6_my_s {
+    // functions
+    #define GO(A, B)    B   A;
+    SUPER()
+    #undef GO
+} libncurses6_my_t;
+
+void* getNCurses6My(library_t* lib)
+{
+    libncurses6_my_t* my = (libncurses6_my_t*)calloc(1, sizeof(libncurses6_my_t));
+    #define GO(A, W) my->A = (W)dlsym(lib->priv.w.lib, #A);
+    SUPER()
+    #undef GO
+    return my;
+}
+#undef SUPER
+
+void freeNCurses6My(void* lib)
+{
+    //libncurses6_my_t *my = (libncurses6_my_t *)lib;
+}
+
+EXPORT void my6_mvwprintw(x86emu_t* emu, void* win, int32_t y, int32_t x, void* fmt, void* b)
+{
+    library_t * lib = GetLibInternal(libncurses6Name);
+    libncurses6_my_t *my = (libncurses6_my_t*)lib->priv.w.p2;
+
+    char* buf = NULL;
+    #ifndef NOALIGN
+    myStackAlign((const char*)fmt, b, emu->scratch);
+    iFppp_t f = (iFppp_t)vasprintf;
+    f(&buf, fmt, emu->scratch);
+    #else
+    iFppp_t f = (iFppp_t)vasprintf;
+    f(&buf, fmt, b);
+    #endif
+    // pre-bake the fmt/vaarg, because there is no "va_list" version of this function
+    my->mvwprintw(win, y, x, buf);
+    free(buf);
+}
+
+#define CUSTOM_INIT \
+    lib->priv.w.p2 = getNCurses6My(lib); \
+    lib->altmy = strdup("my6_");
+
+#define CUSTOM_FINI \
+    freeNCurses6My(lib->priv.w.p2); \
+    free(lib->priv.w.p2);
+
+#include "wrappedlib_init.h"
