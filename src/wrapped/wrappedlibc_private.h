3544428e1816fed194d30bc0e1582117a7157576
Icenowy Zheng <icenowy@aosc.io>
2020-08-02 04:07:29 +0800

    Add an automatic wrapper for {,_}obstack_free
    Signed-off-by: Icenowy Zheng <icenowy@aosc.io>

--- src/wrapped/wrappedlibc_private.h
+++ src/wrapped/wrappedlibc_private.h
@@ -1186,8 +1186,8 @@ DATA(obstack_alloc_failed_handler, 4)
 GOM(_obstack_begin, iFpLLpp)
 // _obstack_begin_1
 DATA(obstack_exit_failure, 4)
-// _obstack_free
-// obstack_free
+GO(_obstack_free, vFpp)
+GO(obstack_free, vFpp)
 // _obstack_memory_used
 GO(_obstack_newchunk, vFpi)
 // obstack_printf   // Weak
