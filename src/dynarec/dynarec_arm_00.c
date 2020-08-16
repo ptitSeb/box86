b302ebfbb5c0e66b2fb07ebab2628df45bbb7be2
ptitSeb <sebastien.chev@gmail.com>
2020-08-16 09:35:40 +0200

    [DYNAREC] Added F2/F3 9C PUSHF

--- src/dynarec/dynarec_arm_00.c
+++ src/dynarec/dynarec_arm_00.c
@@ -2195,6 +2195,7 @@ uintptr_t dynarec00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int ninst,
                     case 0x7D:
                     case 0x7E:
                     case 0x7F:
+                    case 0x9C:
                     case 0xE8:
                     case 0xE9:
                         if(opcode==0xF2) {INST_NAME("BND");} else {INST_NAME("F3");}
