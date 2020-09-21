4241258158dbb899214455ab69f08bfbff0e4e10
ptitSeb <sebastien.chev@gmail.com>
2020-09-21 15:37:23 +0200

    Added a workaround for steam requieremnt checker

--- src/main.c
+++ src/main.c
@@ -836,6 +836,11 @@ int main(int argc, const char **argv, const char **env) {
         AddPath("libudev.so.0", &my_context->box86_emulated_libs, 0);
         box86_steam = 1;
     }
+    // special case for steam-runtime-check-requirements to fake 64bits suport
+    if(strstr(prgname, "steam-runtime-check-requirements")==prgname) {
+        printf_log(LOG_INFO, "steam-runtime-check-requirements detected, faking All is good!\n");
+        exit(0);    // exiting, not testing anything
+    }
     // special case for UnrealLinux.bin, it doesn't like "full path resolution"
     if(!strcmp(prog, "UnrealLinux.bin") && my_context->argv[0]) {
         free(my_context->argv[0]);
