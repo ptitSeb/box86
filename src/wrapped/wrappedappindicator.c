13c446c91836004df56da76973402a57971179d9
ptitSeb <sebastien.chev@gmail.com>
2020-09-21 06:17:08 -0500

    Wrapped libappindicator (seems to fix Steam starting issues)

---
+++ src/wrapped/wrappedappindicator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
+#include <dlfcn.h>
+
+#include "wrappedlibs.h"
+
+#include "wrapper.h"
+#include "bridge.h"
+#include "librarian/library_private.h"
+#include "x86emu.h"
+
+const char* appindicatorName = "libappindicator.so.1";
+#define LIBNAME appindicator
+
+#define CUSTOM_INIT \
+    lib->priv.w.needed = 2; \
+    lib->priv.w.neededlibs = (char**)calloc(lib->priv.w.needed, sizeof(char*)); \
+    lib->priv.w.neededlibs[0] = strdup("libgtk-2.0.so.0");  \
+    lib->priv.w.neededlibs[1] = strdup("libglib-2.0.so.0");
+
+#include "wrappedlib_init.h"
