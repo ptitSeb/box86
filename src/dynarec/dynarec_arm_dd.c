545af93972c951d77ad84e23dfee162edb55e684
ptitSeb <sebastien.chev@gmail.com>
2020-10-10 16:33:10 +0200

    [DYNAREC] Added FBSTSW opcode

--- src/dynarec/dynarec_arm_dd.c
+++ src/dynarec/dynarec_arm_dd.c
@@ -207,6 +207,17 @@ uintptr_t dynarecDD(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int ninst,
                     if(ed!=x1) {MOV_REG(x1, ed);}
                     CALL(arm_fsave, -1, 0);
                     break;
+                case 7:
+                    INST_NAME("FNSTSW m2byte");
+                    fpu_purgecache(dyn, ninst, x1, x2, x3);
+                    addr = geted(dyn, addr, ninst, nextop, &ed, x12, &fixedaddress, 0, 0);
+                    LDR_IMM9(x1, xEmu, offsetof(x86emu_t, top));
+                    MOVW(x2, offsetof(x86emu_t, sw));
+                    ADD_REG_LSL_IMM5(x2, xEmu, x2, 0);
+                    LDRH_IMM8(x3, x2, 0);
+                    BFI(x3, x1, 11, 3); // inject TOP at bit 11 (3 bits)
+                    STRH_IMM8(x3, ed, 0);   // store whole sw flags
+                    break;
                 default:
                     DEFAULT;
             }
