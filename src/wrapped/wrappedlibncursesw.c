f15a22a3cb355bb80b41712f3a4936f3c609b0d8
Bart De Vries <bart@mogwai.be>
2020-10-17 09:34:24 +0200

    Fix presumable typo in wrappedlibncurses files
    A call to vmprintw is made, which is a non-existent function.
    Presumably, vwprintw is meant to be called.

--- src/wrapped/wrappedlibncursesw.c
+++ src/wrapped/wrappedlibncursesw.c
@@ -80,7 +80,7 @@ EXPORT int myw_printw(x86emu_t* emu, void* fmt, void* b)
     myStackAlign((const char*)fmt, b, emu->scratch);
     return my->vwprintw(my->stdscr, fmt, emu->scratch);
     #else
-    return my->vmprintw(my->stdscr, fmt, b);
+    return my->vwprintw(my->stdscr, fmt, b);
     #endif
 }
 
