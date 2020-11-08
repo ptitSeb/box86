6fbd270d2411e6d4661a0205ff4cb80f061caee5
ptitSeb <sebastien.chev@gmail.com>
2020-11-08 13:40:40 +0100

    [DYNAREC] cosmetic change

--- src/dynarec/dynarec_arm_f20f.c
+++ src/dynarec/dynarec_arm_f20f.c
@@ -58,7 +58,7 @@ uintptr_t dynarecF20F(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int nins
     MAYUSE(d0);
 
     switch(nextop) {
-        
+
         case 0x10:
             INST_NAME("MOVSD Gx, Ex");
             nextop = F8;
