adfc5fda2ede4527d38bb18fda753573dd6c088e
ptitSeb <sebastien.chev@gmail.com>
2020-11-22 20:31:33 +0100

    Fixed a SEGBUS on RK339 build

--- src/emu/x86int3.c
+++ src/emu/x86int3.c
@@ -69,6 +69,9 @@ void x86Int3(x86emu_t* emu)
     if(Peek(emu, 0)=='S' && Peek(emu, 1)=='C') // Signature for "Out of x86 door"
     {
         R_EIP += 2;
+        #ifdef RK3399
+        volatile    // to avoid addr to be put in an VFPU register
+        #endif
         uint32_t addr = Fetch32(emu);
         if(addr==0) {
             //printf_log(LOG_INFO, "%p:Exit x86 emu (emu=%p)\n", *(void**)(R_ESP), emu);
