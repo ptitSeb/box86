1eac337e594c351384495ccdddf16da3e954655d
ptitSeb <sebastien.chev@gmail.com>
2020-12-13 16:11:59 +0100

    [DYNAREC] Don't flush dynarec_log

--- src/include/debug.h
+++ src/include/debug.h
@@ -42,7 +42,7 @@ extern FILE* ftrace;
 
 #define printf_log(L, ...) do {if(L<=box86_log) {fprintf(ftrace, __VA_ARGS__); fflush(ftrace);}} while(0)
 
-#define dynarec_log(L, ...) do {if(L<=box86_dynarec_log) {fprintf(ftrace, __VA_ARGS__); fflush(ftrace);}} while(0)
+#define dynarec_log(L, ...) do {if(L<=box86_dynarec_log) {fprintf(ftrace, __VA_ARGS__); /*fflush(ftrace);*/}} while(0)
 
 #define EXPORT __attribute__((visibility("default")))
 #ifdef BUILD_DYNAMIC
