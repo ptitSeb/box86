a19e3ed3dd1ac368afdc4961c65054a45dc33f2f
ptitSeb <sebastien.chev@gmail.com>
2020-12-27 16:11:37 +0100

    [DYNAREC] Small optim on new memory protection tracker

--- src/box86context.c
+++ src/box86context.c
@@ -324,12 +324,13 @@ void protectDB(uintptr_t addr, uintptr_t size)
 {
     uintptr_t idx = (addr>>DYNAMAP_SHIFT);
     uintptr_t end = ((addr+size-1)>>DYNAMAP_SHIFT);
-    for (uintptr_t i=idx; i<=end; ++i) {
-        if(!my_context->memprot[i])
-            my_context->memprot[i] = PROT_READ | PROT_WRITE;    // comes from malloc & co, so should not be able to execute
-        mprotect((void*)(i<<DYNAMAP_SHIFT), 1<<DYNAMAP_SHIFT, my_context->memprot[i]&~PROT_WRITE);
-        my_context->memprot[i] |= PROT_DYNAREC;
-    }
+    for (uintptr_t i=idx; i<=end; ++i)
+        if(!(my_context->memprot[i]&PROT_DYNAREC)) {
+            if(!my_context->memprot[i])
+                my_context->memprot[i] = PROT_READ | PROT_WRITE;    // comes from malloc & co, so should not be able to execute
+            mprotect((void*)(i<<DYNAMAP_SHIFT), 1<<DYNAMAP_SHIFT, my_context->memprot[i]&~PROT_WRITE);
+            my_context->memprot[i] |= PROT_DYNAREC;
+        }
 }
 
 // Add the Write flag from an adress range, and mark all block as dirty
@@ -338,11 +339,12 @@ void unprotectDB(uintptr_t addr, uintptr_t size)
 {
     uintptr_t idx = (addr>>DYNAMAP_SHIFT);
     uintptr_t end = ((addr+size-1)>>DYNAMAP_SHIFT);
-    for (uintptr_t i=idx; i<=end; ++i) {
-        my_context->memprot[i] &= ~PROT_DYNAREC;
-        mprotect((void*)(i<<DYNAMAP_SHIFT), 1<<DYNAMAP_SHIFT, my_context->memprot[i]);
-        cleanDBFromAddressRange(my_context, (i<<DYNAMAP_SHIFT), 1<<DYNAMAP_SHIFT, 0);
-    }
+    for (uintptr_t i=idx; i<=end; ++i)
+        if(my_context->memprot[i]&PROT_DYNAREC) {
+            my_context->memprot[i] &= ~PROT_DYNAREC;
+            mprotect((void*)(i<<DYNAMAP_SHIFT), 1<<DYNAMAP_SHIFT, my_context->memprot[i]);
+            cleanDBFromAddressRange(my_context, (i<<DYNAMAP_SHIFT), 1<<DYNAMAP_SHIFT, 0);
+        }
 }
 
 #endif
