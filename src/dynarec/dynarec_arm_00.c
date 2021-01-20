6c2ebf91c0fd4271d742bf0fb1e1b3d206ecc4a2
ptitSeb <sebastien.chev@gmail.com>
2021-01-20 12:09:16 +0100

    [DYNAREC] Change log level for the Call to 0x0 Warning

--- src/dynarec/dynarec_arm_00.c
+++ src/dynarec/dynarec_arm_00.c
@@ -2026,7 +2026,7 @@ uintptr_t dynarec00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int ninst,
             i32 = F32S;
             if(addr+i32==0) {
                 #if STEP == 3
-                printf_log(LOG_NONE, "Warning, CALL to 0x0 at %p (%p)\n", (void*)addr, (void*)(addr-1));
+                printf_log(LOG_INFO, "Warning, CALL to 0x0 at %p (%p)\n", (void*)addr, (void*)(addr-1));
                 #endif
             }
             #if STEP == 0
