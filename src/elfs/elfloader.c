88a8ebb1dc4dfd17c77f971dfd110f5f84b1f448
ptitSeb <sebastien.chev@gmail.com>
2021-02-22 18:54:34 +0100

    Removed the MAP_FIXED flag to elfloader mmap (maybe need to add a warning if allocated mem is at a different location?)

--- src/elfs/elfloader.c
+++ src/elfs/elfloader.c
@@ -202,7 +202,7 @@ int AllocElfMemory(box86context_t* context, elfheader_t* head, int mainbin)
             printf_log(LOG_DEBUG, "Allocating 0x%x memory @%p for Elf \"%s\"\n", head->multiblock_size[i], (void*)head->multiblock_offs[i], head->name);
             void* p = mmap((void*)head->multiblock_offs[i], head->multiblock_size[i]
                 , PROT_READ | PROT_WRITE | PROT_EXEC
-                , MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED
+                , MAP_PRIVATE | MAP_ANONYMOUS /*| MAP_FIXED*/
                 , -1, 0);
             if(p==MAP_FAILED) {
                 printf_log(LOG_NONE, "Cannot create memory map (@%p 0x%x/0x%x) for elf \"%s\"\n", (void*)head->multiblock_offs[i], head->multiblock_size[i], head->align, head->name);
@@ -218,7 +218,7 @@ int AllocElfMemory(box86context_t* context, elfheader_t* head, int mainbin)
         printf_log(LOG_DEBUG, "Allocating 0x%x memory @%p for Elf \"%s\"\n", head->memsz, (void*)offs, head->name);
         void* p = mmap((void*)offs, head->memsz
             , PROT_READ | PROT_WRITE | PROT_EXEC
-            , MAP_PRIVATE | MAP_ANONYMOUS | (((offs)?MAP_FIXED:0))
+            , MAP_PRIVATE | MAP_ANONYMOUS /*| (((offs)?MAP_FIXED:0))*/
             , -1, 0);
         if(p==MAP_FAILED) {
             printf_log(LOG_NONE, "Cannot create memory map (@%p 0x%x/0x%x) for elf \"%s\"\n", (void*)offs, head->memsz, head->align, head->name);
