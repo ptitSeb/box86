4ca4e84878d6c73792cdd59704472bf76809995d
ptitSeb <sebastien.chev@gmail.com>
2021-06-03 16:15:06 +0200

    Improved elfloader with COPY and GLOBAL reloc with version

--- src/elfs/elfloader.c
+++ src/elfs/elfloader.c
@@ -369,6 +369,7 @@ int FindR386COPYRel(elfheader_t* h, const char* name, uintptr_t *offs, uint32_t*
         const char* symname = SymName(h, sym);
         int version2 = h->VerSym?((Elf32_Half*)((uintptr_t)h->VerSym+h->delta))[ELF32_R_SYM(rel[i].r_info)]:-1;
         if(version2!=-1) version2 &= 0x7fff;
+        if(version && !version2) version2=-1;   // match a versionned symbol against a global "local" symbol
         const char* vername2 = GetSymbolVersion(h, version2);
         if(SameVersionnedSymbol(name, version, vername, symname, version2, vername2) && t==R_386_COPY) {
             *offs = sym->st_value + h->delta;
