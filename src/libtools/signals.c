47c33cd5a9d029e8a0658cfb2be5b1ac882542af
ptitSeb <sebastien.chev@gmail.com>
2021-08-21 17:44:49 +0200

    cancelFillBlock is DYNAREC only (for #443)

--- src/libtools/signals.c
+++ src/libtools/signals.c
@@ -679,9 +679,9 @@ void my_box86signalhandler(int32_t sig, siginfo_t* info, void * ucntx)
 #endif
     int Locks = unlockMutex();
     uint32_t prot = getProtection((uintptr_t)addr);
+#ifdef DYNAREC
     if(Locks & (1<<8) && sig==SIGSEGV) //1<<8 is mutex_dyndump
         cancelFillBlock();  // Segfault inside a Fillblock, just cancel it's creation, don't relock mutex
-#ifdef DYNAREC
     dynablock_t* db = NULL;
     int db_searched = 0;
     if ((sig==SIGSEGV) && (addr) && (info->si_code == SEGV_ACCERR) && (prot&PROT_DYNAREC)) {
