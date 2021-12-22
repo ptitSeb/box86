2e22bb94cd2e34bae08d2873f817991d45966191
Seas0 <30485844+Seas0@users.noreply.github.com>
2021-12-22 17:17:11 +0800

    WIP: Add configration file support

---
+++ src/config.c
@@ -0,0 +1,12 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include "config.h"
+
+// only a dummy file now, wait for implementation
---
+++ src/include/config.h
@@ -0,0 +1,8 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include <stdint.h>
+
+// only a dummy file now, wait for implementation
+
+#endif
