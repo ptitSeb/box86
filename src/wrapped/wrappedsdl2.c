725dabea88f3f7d380504845c63c4401423986f1
Seas0 <30485844+Seas0@users.noreply.github.com>
2021-12-22 17:23:58 +0800

    Move all _GNU_SOURCE defination to the file head Accroding to man 7 feature_test_macros' guide line

--- src/wrapped/wrappedsdl2.c
+++ src/wrapped/wrappedsdl2.c
@@ -1,8 +1,8 @@
+#define _GNU_SOURCE         /* See feature_test_macros(7) */
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define _GNU_SOURCE         /* See feature_test_macros(7) */
 #include <dlfcn.h>
 #include <stdarg.h>
 
