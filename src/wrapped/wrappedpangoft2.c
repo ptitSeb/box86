642bb357d38fc6deb59b98bcf6b312693e86646a
ptitSeb <sebastien.chev@gmail.com>
2022-03-04 22:40:31 +0100

    Put libpangoft2 to the list of gtk wrapped libs

--- src/wrapped/wrappedpangoft2.c
+++ src/wrapped/wrappedpangoft2.c
@@ -6,13 +6,24 @@
 
 #include "wrappedlibs.h"
 
+#include "debug.h"
 #include "wrapper.h"
 #include "bridge.h"
 #include "librarian/library_private.h"
 #include "x86emu.h"
+#include "emu/x86emu_private.h"
+#include "callback.h"
+#include "librarian.h"
+#include "box86context.h"
+#include "emu/x86emu_private.h"
+#include "myalign.h"
 
 const char* pangoft2Name = "libpangoft2-1.0.so.0";
 #define LIBNAME pangoft2
+
+#define PRE_INIT    \
+    if(box86_nogtk) \
+        return -1;
 
 #define CUSTOM_INIT \
     lib->priv.w.needed = 2; \
