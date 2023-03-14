eaa6ecb9cc9d1675cffb1ce8cbf5e3875972a7f0
Alex <40271862+Moldytzu@users.noreply.github.com>
2023-03-14 17:06:54 +0200

    steam: install mesa for armhf if present for arm64 (#786)
    * steam: install mesa for armhf if present for arm64
    
    * fix a copy-pasting issue

--- install_steam.sh
+++ install_steam.sh
@@ -28,14 +28,21 @@ export DBUS_FATAL_WARNINGS=0
 chmod +x steam
 sudo mv steam /usr/local/bin/
 
-# detect if we're running on 64 bit Debian 
+# detect if we're running on 64 bit Debian (maybe this can be moved in another script then mentioned in documentation for the people that don't use steam but don't want a headache)
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'aarch64' ] && [ -f '/etc/debian_version' ]; then
  echo "Detected 64 bit ARM Debian. Installing 32 bit libraries"
  sudo dpkg --add-architecture armhf # enable installation of armhf libraries
  sudo apt update # update package lists with the newly added arch
  # install the libraries that Steam requires
- sudo apt install libc6:armhf libncurses5:armhf libsdl2-2.0-0:armhf libsdl2-image-2.0-0:armhf libsdl2-mixer-2.0-0:armhf libsdl2-ttf-2.0-0:armhf libopenal1:armhf libpng16-16:armhf libfontconfig1:armhf libxcomposite1:armhf libbz2-1.0:armhf libxtst6:armhf libcairo2:armhf libxdamage1:armhf
+ sudo apt install libc6:armhf libncurses5:armhf libsdl2-2.0-0:armhf libsdl2-image-2.0-0:armhf libsdl2-mixer-2.0-0:armhf libsdl2-ttf-2.0-0:armhf libopenal1:armhf libpng16-16:armhf libfontconfig1:armhf libxcomposite1:armhf libbz2-1.0:armhf libxtst6:armhf
+
+ # install mesa for armhf if already installed
+ if dpkg-query -W libgl1-mesa-dri 2>/dev/null; then
+  echo "Detected mesa driver for 64 bit ARM. Installing complementary 32 bit one"
+  sudo apt install libgl1-mesa-dri:armhf
+ fi
+
  echo "Don't forget to compile/install Box64!"
 fi
 
