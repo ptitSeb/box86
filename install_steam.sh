86445adc7cbf6dbebcbbd319a8ac3be2599c5919
Ilya114 <93242944+Ilya114@users.noreply.github.com>
2023-05-16 14:39:14 +0300

    -noreactlogin deleted from Steam

--- install_steam.sh
+++ install_steam.sh
@@ -22,7 +22,7 @@ echo "#!/bin/bash
 export STEAMOS=1
 export STEAM_RUNTIME=1
 export DBUS_FATAL_WARNINGS=0
-~/steam/bin/steam -noreactlogin steam://open/minigameslist $@" > steam
+~/steam/bin/steam steam://open/minigameslist $@" > steam
 
 # make script executable and move
 chmod +x steam
