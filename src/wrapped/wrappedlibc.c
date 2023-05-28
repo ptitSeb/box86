1988eb0366d34d4dbb4bcbc7ba32ac3b579ba2ea
ptitSeb <sebastien.chev@gmail.com>
2023-05-28 14:37:26 +0200

    [DYNAREC] Do not track memory that is not accessible (should save some memory)

--- src/wrapped/wrappedlibc.c
+++ src/wrapped/wrappedlibc.c
@@ -2916,7 +2916,7 @@ EXPORT void* my_mmap(x86emu_t* emu, void *addr, unsigned long length, int prot, 
     }
     #endif
     if(ret!=(void*)-1)
-        setProtection((uintptr_t)ret, length, prot);
+        setProtection_mmap((uintptr_t)ret, length, prot);
     return ret;
 }
 
@@ -2974,7 +2974,7 @@ EXPORT void* my_mmap64(x86emu_t* emu, void *addr, unsigned long length, int prot
     }
     #endif
     if(ret!=(void*)-1)
-        setProtection((uintptr_t)ret, length, prot);
+        setProtection_mmap((uintptr_t)ret, length, prot);
     return ret;
 }
 
@@ -2989,7 +2989,7 @@ EXPORT void* my_mremap(x86emu_t* emu, void* old_addr, size_t old_size, size_t ne
     uint32_t prot = getProtection((uintptr_t)old_addr)&~PROT_CUSTOM;
     if(ret==old_addr) {
         if(old_size && old_size<new_size) {
-            setProtection((uintptr_t)ret+old_size, new_size-old_size, prot);
+            setProtection_mmap((uintptr_t)ret+old_size, new_size-old_size, prot);
             #ifdef DYNAREC
             if(box86_dynarec)
                 addDBFromAddressRange((uintptr_t)ret+old_size, new_size-old_size);
@@ -3001,7 +3001,7 @@ EXPORT void* my_mremap(x86emu_t* emu, void* old_addr, size_t old_size, size_t ne
                 cleanDBFromAddressRange((uintptr_t)ret+new_size, old_size-new_size, 1);
             #endif
         } else if(!old_size) {
-            setProtection((uintptr_t)ret, new_size, prot);
+            setProtection_mmap((uintptr_t)ret, new_size, prot);
             #ifdef DYNAREC
             if(box86_dynarec)
                 addDBFromAddressRange((uintptr_t)ret, new_size);
@@ -3019,7 +3019,7 @@ EXPORT void* my_mremap(x86emu_t* emu, void* old_addr, size_t old_size, size_t ne
                 cleanDBFromAddressRange((uintptr_t)old_addr, old_size, 1);
             #endif
         }
-        setProtection((uintptr_t)ret, new_size, prot); // should copy the protection from old block
+        setProtection_mmap((uintptr_t)ret, new_size, prot); // should copy the protection from old block
         #ifdef DYNAREC
         if(box86_dynarec)
             addDBFromAddressRange((uintptr_t)ret, new_size);
@@ -3058,8 +3058,15 @@ EXPORT int my_mprotect(x86emu_t* emu, void *addr, unsigned long len, int prot)
             cleanDBFromAddressRange((uintptr_t)addr, len, 1);
     }
     #endif
-    if(!ret && len)
-        updateProtection((uintptr_t)addr, len, prot);
+    if(!ret && len) {
+        if(prot)
+            updateProtection((uintptr_t)addr, len, prot);
+        else {
+            // avoid allocating detailled protection for a no prot 0
+            freeProtection((uintptr_t)addr, len);
+            setProtection_mmap((uintptr_t)addr, len, prot);
+        }
+    }
     return ret;
 }
 
