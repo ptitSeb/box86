dad0e5ef2f4bab9fb3bc5fd49678113b217c9ed6
ptitSeb <sebastien.chev@gmail.com>
2023-11-15 14:46:59 +0100

    [DYNAREC] optimized C0 /5 and D0 /5 opcodes

--- src/dynarec/dynarec_arm_00.c
+++ src/dynarec/dynarec_arm_00.c
@@ -1468,19 +1468,16 @@ uintptr_t dynarec00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int ninst,
                     break;
                 case 5:
                     INST_NAME("SHR Eb, Ib");
-                    SETFLAGS(X_ALL, SF_PENDING);
-                    GETEB(x1);
-                    u8 = (F8)&0x1f;
-                    UFLAG_IF{
-                        MOV32(x14, u8); UFLAG_OP2(x14)
-                    };
-                    UFLAG_OP1(ed);
-                    if(u8) {
-                        MOV_REG_LSR_IMM5(ed, ed, u8);
+                    if(geted_ib(dyn, addr, ninst, nextop)&0x1f) {
+                        SETFLAGS(X_ALL, SF_SET_PENDING);    // some flags are left undefined
+                        GETEB(x1);
+                        u8 = (F8)&0x1f;
+                        emit_shr8c(dyn, ninst, ed, u8, x2, x14);
                         EBBACK;
+                    } else {
+                        FAKEED;
+                        F8;
                     }
-                    UFLAG_RES(ed);
-                    UFLAG_DF(x3, d_shr8);
                     break;
                 case 7:
                     INST_NAME("SAR Eb, Ib");
@@ -1811,14 +1808,10 @@ uintptr_t dynarec00(dynarec_arm_t* dyn, uintptr_t addr, uintptr_t ip, int ninst,
                     break;
                 case 5:
                     INST_NAME("SHR Eb, 1");
-                    MOVW(x2, 1);
-                    SETFLAGS(X_ALL, SF_PENDING);
+                    SETFLAGS(X_ALL, SF_SET_PENDING);    // some flags are left undefined
                     GETEB(x1);
-                    UFLAG_OP12(ed, x2)
-                    MOV_REG_LSR_REG(ed, ed, x2);
+                    emit_shr8c(dyn, ninst, ed, 1, x2, x14);
                     EBBACK;
-                    UFLAG_RES(ed);
-                    UFLAG_DF(x3, d_shr8);
                     break;
                 case 7:
                     INST_NAME("SAR Eb, 1");
